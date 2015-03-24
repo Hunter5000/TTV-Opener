d77a5513c904b46832316d9f199faf31f8a70667
Hunter <hunter5000@mailinator.com>
2015-03-24 17:29:59 -0400

    Added ability to open twitch page

--- ttv opener.py
+++ ttv opener.py
@@ -2,7 +2,7 @@ import easygui as eg
 import webbrowser
 import os
 
-def openChat(url, chat, quality):
+def openChat(url, chat, page, quality):
 	check1 = url.find("http")
 	qual = " best"
 	if quality.find("wo") != -1:
@@ -20,23 +20,35 @@ def openChat(url, chat, quality):
 	if quality.find("mo") != -1:
 		qual = " mobile"
 	ch = False
+	pg = False
 	if chat.find("y") != -1:
 		ch = True
+	if page.find("y") != -1:
+		pg = True
 	if check1 != -1:
+		pgurl = (url)
 		newurl = (url + "/chat")
+		if pg == True:
+			webbrowser.open_new(pgurl)
 		if ch == True:
 			webbrowser.open_new(newurl)
 		os.system("livestreamer " + newurl + qual)
 		return "Stream closed"	
 	check2 = url.find("twitch.tv")
 	if check2 != -1:
+		pgurl = ("http://" + url)
 		newurl = ("http://" + url + "/chat")
+		if pg == True:
+			webbrowser.open_new(pgurl)
 		if ch == True:
 			webbrowser.open_new(newurl)
 		os.system("livestreamer " + newurl + qual)
 		return "Stream closed"
 	if check1 == -1 and check2 == -1:
+		pgurl = ("http://twitch.tv/" + url)
 		newurl = ("http://twitch.tv/" + url + "/chat")
+		if pg == True:
+			webbrowser.open_new(pgurl)
 		if ch == True:
 			webbrowser.open_new(newurl)
 		os.system("livestreamer " + newurl + qual)
@@ -46,11 +58,11 @@ def openChat(url, chat, quality):
 while 1:
     msg = "Open the given stream using Livestreamer and its chat in a new browser tab"
     title = "TTV Opener"
-    fieldNames = ["Stream", "Chat (y/n)", "Quality"]
+    fieldNames = ["Stream", "Chat (y/n)","Twitch page (y/n)", "Quality"]
     fieldValues = []
     fieldValues = eg.multenterbox(msg,title, fieldNames)
 
-    message = openChat(fieldValues[0],fieldValues[1], fieldValues[2])
+    message = openChat(fieldValues[0],fieldValues[1], fieldValues[2], fieldValues[3])
 
     eg.msgbox(message)
 
